e7db23cbe7d693e0597997028d4de387a2650cdf
Sergey Lakeev <sl5564@mail.ru>
2020-12-24 18:27:21 +0300

    Change MonkeyTask

--- MonkeyTask/MonkeyTask.cs
+++ MonkeyTask/MonkeyTask.cs
@@ -10,37 +10,33 @@ public class MonkeyTask{
 
 	public static void Main(String[] args){
 		int n = 0;//Хранит кол-во обезьян в зоопарке 
-		while(true){
-			try{
-				n = Convert.ToInt32(Console.ReadLine());//Ввод данных о количестве обезьян				
-				break;
-			}catch{
-				Console.Write("Введите количество обезьян должно быть целочисленным.");
-				return;
-			}
+		try{
+			n = Convert.ToInt32(Console.ReadLine());//Ввод данных о количестве обезьян				
+		}catch{
+			Console.Write("Введите количество обезьян должно быть целочисленным.");
+			goto _End;
 		}
 		int[] MON = new int [n];
 		int[] BAN = new int [n];
 		for(int i=0;i<n;i++){
-			while(true){
-				try{
-					int [] MAS = Peek(Console.ReadLine());//Получаю входные данные в приятном виде
-					MON[i]=MAS[0];
-					BAN[i]=MAS[0]+MAS[1];
-					break;
-				}catch{
-					Console.WriteLine("Неверный форма ввода данных.\nИспользуйте пробел между значениями.");
-					return;
-				}
+			try{
+				int [] MAS = Peek(Console.ReadLine());//Получаю входные данные в приятном виде
+				MON[i]=MAS[0];
+				BAN[i]=MAS[0]+MAS[1];
+			}catch{
+				Console.WriteLine("Неверный форма ввода данных.\nИспользуйте пробел между значениями.");
+				goto _End;
 			}
 		}
 		for(int i=0;i<n;i++){
 			for(int j=0;j<n;j++){
-				if(MON[i]==BAN[j]&&MON[j]==BAN[i]) goto _End;//Проверка, есть ли обезьяны, которые попали друг в друга
+				if(i==j)continue;
+				if(MON[i]==BAN[j]&&MON[j]==BAN[i]) goto _Yes;//Проверка, есть ли обезьяны, которые попали друг в друга
 			}	
 		}
 		Console.WriteLine("NO");
-		return;
-	   _End:Console.WriteLine("YES");
+		goto _End;
+	   _Yes:Console.WriteLine("YES");
+           _End:Console.ReadKey();
 	}
 }
